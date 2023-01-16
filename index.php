7a768814dfa10a69e15a0734c0741cc3c23baab7
Lazar Radosavljevic <radosavljeviclazar81@gmail.com>
2023-01-16 15:06:13 +0100

    Incijalna struktura projekta

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prijava korisnika</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <form method="post">
+        <h2>PRIJAVI SE NA SISTEM</h2>
+    <label for="ime">Korisnicko ime</label>
+    <input type="text" name="kor_ime" id="ime">
+
+    <label for="sifra">Sifra</label>
+    <input type="password" name="sifra" id="sifra">
+
+    <button type="submit">Uloguj se</button>
+    </form>
+</body>
+</html>
---
+++ izdavanje_zahteva.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 16, 2023 at 02:57 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `izdavanje_zahteva`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korisnik`
+--
+
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL,
+  `korisnicko_ime` varchar(255) NOT NULL,
+  `sifra` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+INSERT INTO `korisnik` (`id`, `korisnicko_ime`, `sifra`) VALUES
+(1, 'lazar', 'lazar'),
+(2, 'antonije', 'antonije');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zahtevi_korisnika`
+--
+
+CREATE TABLE `zahtevi_korisnika` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(255) NOT NULL,
+  `prezime` varchar(255) NOT NULL,
+  `nacionalnost` varchar(255) NOT NULL,
+  `grad` varchar(255) NOT NULL,
+  `zahtev` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `zahtevi_korisnika`
+--
+ALTER TABLE `zahtevi_korisnika`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `zahtevi_korisnika`
+--
+ALTER TABLE `zahtevi_korisnika`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
